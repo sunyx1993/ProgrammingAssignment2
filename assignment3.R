0c8a62b4f2c9a18515c839b611e15d01d0e9de62
sunyx1993 <48975981+sunyx1993@users.noreply.github.com>
2019-06-09 21:08:50 +0800

    Update assignment3.R

--- assignment3.R
+++ assignment3.R
@@ -1,175 +1,129 @@
-#Programming Assignment 3: Hospital Quality: Instructions
-#get current working directory
+# Programming Assignment 2: Lexical Scoping
+# Introduction
+# This second programming assignment will require you to write an R function is 
+# able to cache potentially time-consuming computations. For example, taking 
+# the mean of a numeric vector is typically a fast operation. However, for 
+# a very long vector, it may take too long to compute the mean, especially 
+# if it has to be computed repeatedly (e.g. in a loop). If the contents of 
+# a vector are not changing, it may make sense to cache the value of the mean 
+# so that when we need it again, it can be looked up in the cache rather than 
+# recomputed. In this Programming Assignment will take advantage of the scoping 
+# rules of the R language and how they can be manipulated to preserve state 
+# inside of an R object.
+
 getwd()
 # set the working directpory to github repo
 setwd("~/Documents/Repos/datasciencecoursera/RProgramming")
 #check the content of the folder
 dir()
 
-# Downloaded file is located in Data folder, check the content
-dir("Data/")
-# If the file is not extracted, run this code to extract it
-unzip("Data/ProgAssignment3-data.zip", exdir = "Data")
-# Now check if the extraction was successful
-dir("Data/")
+# Example: Caching the Mean of a Vector
 
-### Part 1
-outcome <- read.csv("Data/outcome-of-care-measures.csv", colClasses = "character")
-head(outcome)
-ncol(outcome)
-nrow(outcome)
-dim(outcome)
-names(outcome)
-outcome[, 11] <- as.numeric(outcome[, 11])
-hist(outcome[,11])
+# In this example we introduce the <<- operator which can be used to assign 
+# a value to an object in an environment that is different from the current 
+# environment. Below are two functions that are used to create a special 
+# object that stores a numeric vector and cache's its mean.
 
-### Part 2
+# The first function, makeVector creates a special "vector", which is 
+# really a list containing a function to
+# set the value of the vector
+# get the value of the vector
+# set the value of the mean
+# get the value of the mean
 
-best <- function(state, outcome) {
-    ## Read outcome data
-    ## Check that state and outcome are valid 
-    ## Return hospital name in that state with lowest 30-day death rate
-    
-    data <- read.csv("Data/outcome-of-care-measures.csv", colClasses = "character")
-    states <- unique(data$State)
-    #outcomes <- c("heart attack", "heart failure", "pneumonia")
-    
-    if (state %in% states) {
-        if (outcome == "heart attack") {
-            columnNo <- 11
-        } else if (outcome == "heart failure") {
-            columnNo <- 17
-        } else if (outcome == "pneumonia") {
-            columnNo <- 23
-        } else {
-            stop("invalid outcome")
-        }
-        data2 <- data[which(data$State == state), c(2,columnNo)]
-        data2[,2] <- as.numeric(data2[, 2])
-        data2 <- data2[complete.cases(data2),]
-        #data2 <- data2[with(data2, order(2,1)),]
-        data2 <- data2[order(data2[,2], data2[,1]),]
-        data2[1,1]
-    } else {
-        stop("invalid state")
-        #print(paste("Error in best(", state, ", ", outcome, ",) : invalid state", sep = ""))
+
+makeVector <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
     }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
 }
 
-best("TX", "heart attack")
-## [1] "CYPRESS FAIRBANKS MEDICAL CENTER"
-best("TX", "heart failure")
-## [1] "FORT DUNCAN MEDICAL CENTER"
-best("MD", "heart attack")
-## [1] "JOHNS HOPKINS HOSPITAL, THE"
-best("MD", "pneumonia")
-## [1] "GREATER BALTIMORE MEDICAL CENTER"
-best("BB", "heart attack")
-## Error in best("BB", "heart attack") : invalid state
-best("NY", "hert attack")
-## Error in best("NY", "hert attack") : invalid outcome
+# The following function calculates the mean of the special "vector" created 
+# with the above function. However, it first checks to see if the mean has 
+# already been calculated. If so, it gets the mean from the cache and skips 
+# the computation. Otherwise, it calculates the mean of the data and sets the 
+# value of the mean in the cache via the setmean function.
 
-### Part 2
-
-rankhospital <- function(state, outcome, num = "best") {
-    ## Read outcome data
-    ## Check that state and outcome are valid 
-    ## Return hospital name in that state with lowest 30-day death rate
-    
-    data <- read.csv("Data/outcome-of-care-measures.csv", colClasses = "character")
-    states <- unique(data$State)
-    #outcomes <- c("heart attack", "heart failure", "pneumonia")
-    
-    if (state %in% states) {
-        if (outcome == "heart attack") {
-            columnNo <- 11
-        } else if (outcome == "heart failure") {
-            columnNo <- 17
-        } else if (outcome == "pneumonia") {
-            columnNo <- 23
-        } else {
-            stop("invalid outcome")
-        }
-        data2 <- data[which(data$State == state), c(2,columnNo)]
-        data2[,2] <- as.numeric(data2[, 2])
-        data2 <- data2[complete.cases(data2),]
-        #data2 <- data2[with(data2, order(2,1)),]
-        data2 <- data2[order(data2[,2], data2[,1]),]
-        if (num == "best") {
-            data2[1,1]
-        } else if (num == "worst") {
-            data2[nrow(data2), 1]
-        } else if (num <= nrow(data2)) {
-            data2[num, 1]
-        } else {
-            NA
-        }
-    } else {
-        stop("invalid state")
-        #print(paste("Error in best(", state, ", ", outcome, ",) : invalid state", sep = ""))
+cachemean <- function(x, ...) {
+    m <- x$getmean()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
     }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
 }
 
-rankhospital("TX", "heart failure", 4)
-## [1] "DETAR HOSPITAL NAVARRO"
-rankhospital("MD", "heart attack", "worst")
-## [1] "HARFORD MEMORIAL HOSPITAL"
-rankhospital("MN", "heart attack", 5000)
-## [1] NA
-rankhospital("WA", "heart attack", 7)
+a <- makeVector(1:20)
+a$get()
+a$getmean()
+system.time(cachemean(a))
+a$get()
+a$getmean()
 
-## Part 3
 
-rankall <- function(outcome, num = "best") {
-    ## Read outcome data
-    ## Check that state and outcome are valid
-    ## For each state, find the hospital of the given rank
-    ## Return a data frame with the hospital names and the
-    ## (abbreviated) state name
-    
-    data <- read.csv("Data/outcome-of-care-measures.csv", colClasses = "character")
-    states <- unique(data[,7, drop = F])
-    rownames(states) <- states[,1]
-    states <- states[order(states[,1]),, drop = FALSE]
-    states$hospital <- NA
-    colnames(states)[1] <- "state"
-    states <- states[, order(names(states))]
-    for (state in states$state) {
-        if (outcome == "heart attack") {
-            columnNo <- 11
-        } else if (outcome == "heart failure") {
-            columnNo <- 17
-        } else if (outcome == "pneumonia") {
-            columnNo <- 23
-        } else {
-            stop("invalid outcome")
-        }
-        data2 <- data[which(data$State == state), c(2,columnNo)]
-        data2[,2] <- as.numeric(data2[, 2])
-        data2 <- data2[complete.cases(data2),]
-        #data2 <- data2[with(data2, order(2,1)),]
-        data2 <- data2[order(data2[,2], data2[,1]),]
-        if (num == "best") {
-            states[which(states$state == state), 1] <- data2[1,1]
-        } else if (num == "worst") {
-            states[which(states$state == state), 1] <- data2[nrow(data2), 1]
-        } else if (num <= nrow(data2)) {
-            states[which(states$state == state), 1] <- data2[num, 1]
-        } else {
-            states[which(states$state == state), 1] <- NA
-        }
+# Caching the Inverse of a Matrix
+# The overall goal of the below work is to make the process of getting the
+# inverse of a function faster. It is fairly easy to get the inverse of a function
+# in R. In a certain study we might be needing to update a matrix and calculate
+# its inverse many times. The function we are generating here will look at the 
+# environment and find the inverse if it is already calculated and in turn saves
+# us some time.
+
+# The first function, makeCacheMatrix creates a special "matrix", which is 
+# really a list containing a function to
+# set the value of the matrix
+# get the value of the matrix
+# set the value of the inverse
+# get the value of the inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL #inverse of the matrix
+    set <- function(y) { # This part sets the function
+        x <<- y
+        inv <<- NULL
     }
-    states
+    get <- function() x # to get the matrix
+    setInv <- function(solve) inv <<- solve # To set the inverse of the matrix
+    getInv <- function() inv # to get the calculated inverse
+    list(set = set, get = get, setInv = setInv, getInv = getInv) # a special list containing all the sub-functions
 }
 
-head(rankall("heart attack", 20), 10)
-tail(rankall("pneumonia", "worst"), 3)
-tail(rankall("heart failure"), 10)
 
-###############################################################################
-## Submit 
-source("submitscript3.R")
-source("best.R")
-source("rankhospital.R")
-source("rankall.R")
-submit()
+# The following function calculates the inverse of the special "matrix" created 
+# with the above function. However, it first checks to see if the inverse has 
+# already been calculated. If so, it gets the inverse from the cache and skips 
+# the computation. Otherwise, it calculates the inverse of the data and sets the 
+# value of the inverse in the cache via the setInv function.
+
+cacheSolve <- function(x, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInv() # first get the inverse
+    if(!is.null(inv)) { # if the inverse is not Null then return it
+        message("getting cached inverse")
+        return(inv)
+    }
+    data <- x$get() # if the inverse is Null then let's get the matrix 
+    inv <- solve(data, ...) # and pass the matrix to calculation
+    x$setInv(inv) # assign the calculated value to the inverse
+    inv # auto - print the result
+}
+
+mat <- matrix(rnorm(1000000), 1000, 1000)
+system.time(solve(mat))
+mm <- makeCacheMatrix(mat)
+mm$get()
+mm$getInv()
+system.time(cacheSolve(mm))
+mm$get()
+mm$getInv()
